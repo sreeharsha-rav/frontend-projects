22e8aaaeb5612991ad246d152f397300f6768a8d
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-08-03 05:47:18 -0700

    added material to angular-apps/projects/todo-list

--- angular-apps/projects/todo-list/src/app/app.component.ts
+++ angular-apps/projects/todo-list/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <mat-slide-toggle>Toggle me!</mat-slide-toggle>
     <router-outlet />
   `,
   styles: [],
